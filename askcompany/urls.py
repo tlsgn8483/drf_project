702bfe36d30ac959cb43367fb093824959388d0b
tlsgn8483 <tlsgn511@icloud.com>
2022-07-02 20:26:33 +0900

    인스타그램 app 생성

--- askcompany/urls.py
+++ askcompany/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-auth', include('rest_framework.urls'))
+    path('api-auth/', include('rest_framework.urls')),
+    path('', include('instagram.urls')),
 ]
